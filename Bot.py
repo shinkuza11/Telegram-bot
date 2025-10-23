5cc3eecc383a3ea565eca8937a5ad890c3452e1e
shinkuza11 <spenglermike00@gmail.com>
2025-10-24 00:28:05 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,12 +1,14 @@
 import telebot
 import os
 
-TOKEN = os.getenv("8256155430:AAF-bQpZk7EpBOOQfm1Ubv3h4Ab_lZIrJYQ")  # Bot-Token aus Umgebungsvariable
+# Lade den Bot-Token aus der Render-Umgebungsvariable
+TOKEN = os.getenv("8256155430:AAF-bQpZk7EpBOOQfm1Ubv3h4Ab_lZIrJYQ")
 bot = telebot.TeleBot(TOKEN)
 
+# Wenn jemand /start schreibt
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Hallo 👋 Ich bin online!")
+def start_message(message):
+    bot.reply_to(message, "✅ Test vom Bot – ich bin online!")
 
-print("Bot läuft...")
+print("🤖 Bot läuft ...")
 bot.infinity_polling()
